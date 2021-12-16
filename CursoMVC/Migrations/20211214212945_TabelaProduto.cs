7fa7270feb8bf03ef910d80bb4d20c6c8030cbfd
Ramon Palheta Pessoa <ramon.palheta.pessoa@hotmail.com>
2021-12-16 18:37:59 -0300

    Iniciando os estudos e desenvolvimento .NET com padrao MVC

---
+++ CursoMVC/Migrations/20211214212945_TabelaProduto.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CursoMVC.Migrations
+{
+    public partial class TabelaProduto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Produtos_Produtos_ProdutoId",
+                table: "Produtos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Produtos_ProdutoId",
+                table: "Produtos");
+
+            migrationBuilder.DropColumn(
+                name: "ProdutoId",
+                table: "Produtos");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoriaId",
+                table: "Produtos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantidade",
+                table: "Produtos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_CategoriaId",
+                table: "Produtos",
+                column: "CategoriaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Produtos_Categorias_CategoriaId",
+                table: "Produtos",
+                column: "CategoriaId",
+                principalTable: "Categorias",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Produtos_Categorias_CategoriaId",
+                table: "Produtos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Produtos_CategoriaId",
+                table: "Produtos");
+
+            migrationBuilder.DropColumn(
+                name: "CategoriaId",
+                table: "Produtos");
+
+            migrationBuilder.DropColumn(
+                name: "Quantidade",
+                table: "Produtos");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProdutoId",
+                table: "Produtos",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_ProdutoId",
+                table: "Produtos",
+                column: "ProdutoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Produtos_Produtos_ProdutoId",
+                table: "Produtos",
+                column: "ProdutoId",
+                principalTable: "Produtos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
